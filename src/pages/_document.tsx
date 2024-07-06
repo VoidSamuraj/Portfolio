15b62fe04ba5f9596aa207881af30e752914b4d6
Karol <karol.robak124@gmail.com>
2024-07-06 22:02:16 +0200

    favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head >
-      <link rel="shortcut icon"  type="image/png" href="/favicon.png?" />
+      <link rel="shortcut icon"  type="image/png" href="/favicon.png" />
       </Head>
       <body>
         <Main />

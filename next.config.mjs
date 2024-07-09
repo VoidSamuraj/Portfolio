f12830007a72fdf29b53ca5b673ae63c927a9a79
Karol <karol.robak124@gmail.com>
2024-07-09 14:29:28 +0200

    assetsLink

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = true;
+const basePath = isProd ? '/Portfolio' : '';
 const nextConfig = {
   reactStrictMode: true,
+  basePath: basePath,
+  assetPrefix: isProd ? `https://voidsamuraj.github.io/Portfolio` : '',,
+  output: "export",
 };
 
 export default nextConfig;
